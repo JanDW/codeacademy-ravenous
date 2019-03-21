3a54d1af0a357e6f22d36fb636019c5c9959fd73
Jan De Wilde <jandw@mit.edu>
2019-03-21 14:02:35 -0400

    add .editorconfig and prettier pre-commit hook

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import SearchBar from './components/SearchBar/SearchBar';
 class App extends React.Component {
   render() {
     return (
-			<div className="App">
-			  <h1>ravenous</h1>
-			  <SearchBar />
-			  <BusinessList />
-			</div>
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
     );
   }
 }
